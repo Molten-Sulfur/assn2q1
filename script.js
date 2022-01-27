34b7332ac19b3cd194987cf087776ed948d31f49
Molten-Sulfur <97858539+Molten-Sulfur@users.noreply.github.com>
2022-01-26 18:19:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 var map = L.map('restaurantMap').setView([38.625, -90.286538], 12);
+
+var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(map);
